755f87da42f2107e50331bfefd5059a6be9e9372
Hong Dung <hongdung6992@gmail.com>
2020-09-14 08:53:47 +0700

    fix Notfound page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,27 @@
-import './bootstrap'
+import "./bootstrap";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Notifications from "vue-notification";
 import routes from "./routes";
 import store from "./store";
-import i18n from './lang'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import VueChatScroll from 'vue-chat-scroll'
-import Antd from 'ant-design-vue';
+import i18n from "./lang";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import VueChatScroll from "vue-chat-scroll";
+import Antd from "ant-design-vue";
 
 Vue.use(Antd);
 Vue.use(ElementUI);
 Vue.use(VueRouter);
 Vue.use(Notifications);
-Vue.use(VueChatScroll)
+Vue.use(VueChatScroll);
 
-import Auth from './auth.js';
-Vue.prototype.$auth  = new Auth(store.state.auth);
+import Auth from "./auth.js";
+Vue.prototype.$auth = new Auth(store.state.auth);
 
 import App from "./components/App.vue";
 const router = new VueRouter({
-  baseUrl: '',
+  baseUrl: "",
   routes,
   mode: "history"
 });
@@ -30,5 +30,5 @@ const app = new Vue({
   render: h => h(App),
   i18n,
   store,
-  router,
+  router
 }).$mount("#app");
